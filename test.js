09589f370c95fd07dc163d0dc426b633d1634160
megastef <stefan.thies@sematext.com>
2016-09-05 23:54:04 +0200

    init logagent demo plugin

---
+++ test.js
@@ -0,0 +1,32 @@
+var InputTCP = require('./index.js')
+var net = require('net')
+function test () {
+  var EE = require('events').EventEmitter
+  var config = {
+    configFile: {
+      input: {
+        tcp: {
+          port: 4546,
+          bindAddress: '127.0.0.1',
+          sourceName: 'test',
+          debug: true
+        }
+      }
+    }
+  }
+  var plugin = new InputTCP(config, new EE())
+  plugin.start()
+  // simulate a client
+  setTimeout(function () {
+    var client = new net.Socket()
+    client.connect(config.configFile.input.tcp.port, '127.0.0.1', function () {
+      console.log('client connected')
+      client.write('Hello, server!')
+      client.end()
+    })
+  }, 1000)
+}
+
+if (require.main === module && process.argv[2] == 'test') {
+  test()
+}
